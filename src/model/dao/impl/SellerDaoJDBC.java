af7df66296b7cab6543b7318b45720e3e092ebe5
Paulo Fernandes <marciorocha11@gmail.com>
2021-12-23 19:00:04 -0200

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -90,6 +93,47 @@ public class SellerDaoJDBC implements SellerDao {
 		dep.setName(rs.getString("DepName"));
 		return dep;
 		//instanciando um departamento e instanciando os dados do banco de dados no objeto departamento.
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"Select seller.*, department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE Department.Id = ? "
+					+ "ORDER BY Name");
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>(); // map criado para guardar qualquer departamento 
+			
+			while(rs.next()){
+				Department dep = map.get(rs.getInt("DepartmentId")); // verificar se o departamento ja existe
+				
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+					
+					// teste de validação de existencia do Departamento, se for verdadeiro, salva dentro do map para proxima validação
+				}
+				Seller obj = instantiateSeller(rs,dep);
+				list.add(obj);
+			}
+			return list;
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 
